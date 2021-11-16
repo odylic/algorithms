9a94d0582ac4e447d8ec0a850e8a7c971c7fe3ea
Jimmy <odylic.lin@gmail.com>
2021-11-15 18:44:54 -0800

    remove

--- linked-list-remove.js
+++ linked-list-remove.js
@@ -41,6 +41,57 @@ Given a linked list:
 
 */
 
+
+
+// module.exports = {
+//   LinkedList,
+//   Node,
+//   linkedListRemove,
+//   linkedListRemoveMultiple,
+// };
+
+// declare a ll with new LinkedList constructor
+const ll = new LinkedList();
+// declare a nodeList with new Node constructor
+const nodeList = new Node("a");
+// declare the next values in nodeList next
+nodeList.next = new Node("b");
+nodeList.next.next = new Node("c");
+nodeList.next.next.next = new Node("b");
+nodeList.next.next.next.next = new Node("d");
+// declare the head of ll to nodeList
+ll.head = nodeList;
+
+console.log(ll);
+
+const linkedListRemove = (ll, val) => {
+  // removes
+  if (ll.head.val === val) {
+    ll.head = ll.head.next;
+    return ll;
+  }
+
+  let prev = ll.head;
+  let curr = ll.head.next;
+  // while there is a next
+  while (curr) {
+    // if it's the val
+    if (curr.val === val) {
+      // remove the curr, make ll.head.next = ll.head.next.next
+      // which removes current which was ll.head.next
+      prev.next = curr.next;
+      return ll;
+    }
+    // move the pointers along
+    prev = curr;
+    curr = curr.next;
+  }
+  return ll;
+};
+
+// console.log(linkedListRemove(ll, "b"));
+
+
 const linkedListRemoveMultiple = (ll, val) => {
   // if head.val is value, return next, skips over first
   if (ll.head.val === val) return ll.head.next;
@@ -68,82 +119,4 @@ const linkedListRemoveMultiple = (ll, val) => {
   return ll;
 };
 
-// module.exports = {
-//   LinkedList,
-//   Node,
-//   linkedListRemove,
-//   linkedListRemoveMultiple,
-// };
-
-// declare a ll with new LinkedList constructor
-const ll = new LinkedList();
-// declare a nodeList with new Node constructor
-const nodeList = new Node("a");
-// declare the next values in nodeList next
-nodeList.next = new Node("b");
-nodeList.next.next = new Node("c");
-nodeList.next.next.next = new Node("b");
-nodeList.next.next.next.next = new Node("d");
-// declare the head of ll to nodeList
-ll.head = nodeList;
-
-console.log(ll);
-
-// const linkedListRemove = (ll, val) => {
-//   // if the head is the value
-//   if (ll.head.val === val) {
-//     // have this head skipped to the next
-//     ll.head = ll.head.next;
-//     // return ll
-//     return ll;
-//   }
-
-//   // previous is assigned to head
-//   let prev = ll.head;
-//   // current is previous.next
-//   let curr = ll.head.next;
-
-//   // then keep track of the previous with current
-//   while (curr) {
-//     // if current is the value
-//     if (curr.val === val) {
-//       // previous.next is reassigned current's next
-//       // skips current
-//       prev.next = curr.next;
-//       // return ll
-//       return ll;
-//     }
-
-//     prev = curr;
-//     curr = curr.next;
-//   }
-//   return ll;
-// };
-
-const linkedListRemove = (ll, val) => {
-  // removes
-  if (ll.head.val === val) {
-    ll.head = ll.head.next;
-    return ll;
-  }
-
-  let prev = ll.head;
-  let curr = ll.head.next;
-  // while there is a next
-  while (curr) {
-    // if it's the val
-    if (curr.val === val) {
-      // remove the curr, make ll.head.next = ll.head.next.next
-      // which removes current which was ll.head.next
-      prev.next = curr.next;
-      return ll;
-    }
-    // move the pointers along
-    prev = curr;
-    curr = curr.next;
-  }
-  return ll;
-};
-
-console.log(linkedListRemove(ll, "b"));
-// console.log(linkedListRemoveMultiple(ll, "b"));
+console.log(linkedListRemoveMultiple(ll, "b"));
